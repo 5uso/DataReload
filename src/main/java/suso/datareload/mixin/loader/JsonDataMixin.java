4b2580e561c6eaa0d89844d4284a3de4f3fb80ec
Suso <jesusmosteiro@gmail.com>
2023-09-01 17:10:49 +0200

    [1.6] Update to 1.20.1

--- src/main/java/suso/datareload/mixin/loader/JsonDataMixin.java
+++ src/main/java/suso/datareload/mixin/loader/JsonDataMixin.java
@@ -1,5 +1,6 @@
 package suso.datareload.mixin.loader;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import net.minecraft.resource.JsonDataLoader;
 import net.minecraft.resource.Resource;
@@ -10,24 +11,20 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import suso.datareload.Utility;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.Reader;
 import java.util.Iterator;
 import java.util.Map;
 
 @Mixin(JsonDataLoader.class)
 public class JsonDataMixin {
     @Inject(
-            method = "Lnet/minecraft/resource/JsonDataLoader;prepare(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)Ljava/util/Map;",
+            method = "load",
             at = @At(
                     value = "INVOKE",
                     target = "Lorg/slf4j/Logger;error(Ljava/lang/String;[Ljava/lang/Object;)V",
@@ -35,7 +32,7 @@ public class JsonDataMixin {
             ),
             locals = LocalCapture.CAPTURE_FAILSOFT
     )
-    public void parseError(ResourceManager resourceManager, Profiler profiler, CallbackInfoReturnable<Map<Identifier, JsonElement>> cir, Map<Identifier, JsonElement> map, ResourceFinder resourceFinder, Iterator<Map.Entry<Identifier, Resource>> var5, Map.Entry<Identifier, Resource> entry, Identifier identifier, Identifier identifier2, Exception var15) {
+    private static void parseError(ResourceManager manager, String dataType, Gson gson, Map<Identifier, JsonElement> results, CallbackInfo ci, ResourceFinder resourceFinder, Iterator<Map.Entry<Identifier, Resource>> var5, Map.Entry<Identifier, Resource> entry, Identifier identifier, Identifier identifier2, Exception var15) {
         Text t = MutableText.of(new LiteralTextContent("\n"))
                 .append(Utility.strToText("- Couldn't parse data file ", Formatting.RED))
                 .append(Utility.strToText(identifier2.toString(), Formatting.AQUA))
--- src/main/java/suso/datareload/mixin/loader/LootMixin.java
+++ src/main/java/suso/datareload/mixin/loader/LootMixin.java
@@ -1,38 +1,32 @@
 package suso.datareload.mixin.loader;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.gson.JsonElement;
 import net.minecraft.loot.LootManager;
-import net.minecraft.loot.LootTable;
 import net.minecraft.text.LiteralTextContent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import suso.datareload.Utility;
 
 @Mixin(LootManager.class)
 public class LootMixin {
     @Inject(
-            method = "method_20711",
+            method = "method_51197",
             at = @At(
                     value = "INVOKE",
-                    target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V",
+                    target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V",
                     remap = false
-            ),
-            locals = LocalCapture.CAPTURE_FAILSOFT
+            )
     )
-    private static void error(ImmutableMap.Builder<Identifier, LootTable> builder, Identifier id, JsonElement json, CallbackInfo ci, Exception var4) {
+    private static void error(String name, String message, CallbackInfo ci) {
         Text t = MutableText.of(new LiteralTextContent("\n"))
-                .append(Utility.strToText("- Couldn't parse loot table ", Formatting.RED))
-                .append(Utility.strToText(id.toString(), Formatting.AQUA))
+                .append(Utility.strToText("- Validation problem in loot table ", Formatting.RED))
+                .append(Utility.strToText(name, Formatting.AQUA))
                 .append(Utility.strToText("\n "))
-                .append(Utility.strToText(var4.getMessage()));
+                .append(Utility.strToText(message));
         Utility.sendMessage(t);
     }
 }
